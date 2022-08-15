d7dba0486a5b6fb38e19a2c708e2e987cb7a0ebb
Mr Jerz <mrjerz@pm.me>
2022-08-15 19:39:23 +0200

    fix install script

--- install.sh
+++ install.sh
@@ -4,8 +4,9 @@ set -e
 sudo rm -f /etc/bashrc.backup-before-nix
 sudo rm -f /etc/zshrc.backup-before-nix
 sudo rm -f /etc/bash.bashrc.backup-before-nix
-curl -L https://releases.nixos.org/nix/nix-2.6.1/install | sh
+curl -L https://nixos.org/nix/install | sh
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --add https://nixos.org/channels/nixos-unstable
 nix-channel --update
 export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
 nix-shell '<home-manager>' -A install
