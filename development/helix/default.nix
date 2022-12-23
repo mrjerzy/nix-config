8bc26d9e05c042f5c660ea3386fdc5470c7f15ab
Mr Jerz <mrjerz@pm.me>
2022-12-23 11:06:02 +0100

    Update helix configuration

--- development/helix/default.nix
+++ development/helix/default.nix
@@ -19,12 +19,16 @@
     programs.helix = {
       enable = true;
       settings = {
-        theme = "base16_terminal";
+        theme = "base16_default";
+        editor.file-picker.hidden = false;
+        editor.line-number = "relative";
+        editor.cursor-shape.insert = "bar";
         keys.normal = {
           space.space = "file_picker";
           ",".q = ":q";
           ",".w = ":w";
           "0" = "goto_line_start";
+          "`" = "keep_primary_selection";
 
           # Update configuration with home-manager
           ","."`" = ":run-shell-command home-manager switch --flake $NIX_MACHINE_CFG";
