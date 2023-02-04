1dafd8f247fe6789d89225acc648bda44f90470d
Mr Jerz <mrjerz@pm.me>
2023-02-04 16:19:21 +0100

    Add kubernetes tooling

--- development/k8s/default.nix
+++ development/k8s/default.nix
@@ -7,9 +7,36 @@
       pkgs.kubectl
       pkgs.minikube
       pkgs.kustomize
+      pkgs.kubectx
+      pkgs.argocd
+      pkgs.kubernetes-helm
+      pkgs.kubeseal
+      unixtools.watch
     ];
+
+    programs.zsh.shellAliases = {
+      k = "kubectl";
+      kgd = "kubectl get deploy";
+      kgp = "kubectl get pod";
+      kg = "kubectl get";
+      kd = "kubectl describe";
+      ke = "kubectl explain";
+      ker = "kubectl explain --recursive";
+      ka = "kubectl apply -f";
+    };
 
     home.sessionVariables = {
     };
+
+    programs.zsh.initExtra = ''
+      source <($HOME/.nix-profile/bin/kubectl completion zsh)    
+      source <($HOME/.nix-profile/bin/kustomize completion zsh)    
+      source <($HOME/.nix-profile/bin/argocd completion zsh)    
+      source <($HOME/.nix-profile/bin/helm completion zsh)    
+
+      # Kubernetes Commands
+      dy=(--dry-run=client -o yaml)
+      y=(-o yaml)
+    '';
   };
 }
