6ef9f14e096fe0d67b159454c61f4dfe39d340c3
Mr Jerz <mrjerz@pm.me>
2023-03-11 14:20:08 +0100

    Switch back to vim

--- home.nix
+++ home.nix
@@ -16,8 +16,8 @@
 
   # Environment Variables
   home.sessionVariables = {
-    K9S_EDITOR = "$HOME/.nix-profile/bin/hx";
-    EDITOR = "$HOME/.nix-profile/bin/hx";
+    K9S_EDITOR = "$HOME/.nix-profile/bin/nvim";
+    EDITOR = "$HOME/.nix-profile/bin/nvim";
     NIX_PATH = "$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels";
     CFG = "$HOME/nix-config/";
     NIX_MACHINE_CFG = "$CFG#$NIX_MACHINE";
@@ -37,7 +37,7 @@
     l = "exa -l";
     "," = "nix shell nixpkgs#";
     bc = "bc -l";
-    cfg = "cd $CFG && hx home.nix";
+    cfg = "cd $CFG && nvim home.nix";
     ta = "tmux attach";
     du = "dust";
     df = "duf";
