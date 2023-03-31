a39a3e1861e4e790b6afcf4d53cbffb1f90822e4
Mr Jerz <mrjerz@pm.me>
2023-03-31 09:15:53 +0200

    Add neovim treesitter support

--- development/neovim/default.nix
+++ development/neovim/default.nix
@@ -69,6 +69,81 @@
           '';
         }
         {
+          plugin = nvim-treesitter;
+          config = ''
+            lua <<EOF
+            require'nvim-treesitter.configs'.setup {
+              parser_install_dir = "$HOME/.local/share/nvim/site";
+              auto_install = true;
+
+              highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+              };
+
+              ensure_installed = {
+                "bash",
+                "c",
+                "diff",
+                "dockerfile",
+                "dot",
+                "git_rebase",
+                "gitattributes",
+                "gitcommit",
+                "gitignore",
+                "go",
+                "gomod",
+                "gowork",
+                "haskell",
+                "html",
+                "http",
+                "java",
+                "json",
+                "jsonnet",
+                "lua",
+                "make",
+                "markdown",
+                "markdown_inline",
+                "mermaid",
+                "org",
+                "python",
+                "r",
+                "rst",
+                "rust",
+                "sql",
+                "terraform",
+                "toml",
+                "vim",
+                "yaml"
+              }
+            }
+            EOF
+          '';
+        }
+        nvim-treesitter-context
+        {
+          plugin = nvim-cmp;
+          config = ''
+          lua <<EOF
+          require'cmp'.setup {
+            }
+          EOF
+          '';
+        }
+        nvim-treesitter-context
+        {
+          plugin = nvim-treesitter;
+          config = ''
+          lua <<EOF
+            require'cmp'.setup {
+              sources = {
+                { name = 'treesitter' }
+              }
+            }
+          EOF
+          '';
+        }
+        {
           plugin = comment-nvim;
           config = ''
             lua require('Comment').setup()
